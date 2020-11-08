2f63ad65a9df85f4453e78210257a7749b0f12ba
reshchat <55682533+reshchat@users.noreply.github.com>
2020-11-08 20:19:17 +0530

    Create New.java

---
+++ New.java
@@ -0,0 +1,59 @@
+package application;
+
+import java.io.FileInputStream;
+
+import java.io.FileNotFoundException;
+import java.util.Random;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.shape.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+	@Override
+	public void start(Stage theStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+}
+class Homepage{
+	
+}
+class Game{
+	
+}
+class Ball{
+	
+}
+class Player{
+	
+}
+class ColourChanger{
+	
+}
+abstract class Obstacle{
+	
+}
+
