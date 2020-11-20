b532fb1563a276d5b4edb3b2138a3dcc3f07d8a4
kmn01 <55681293+kmn01@users.noreply.github.com>
2020-11-21 02:50:20 +0530

    Update New.java

--- New.java
+++ New.java
@@ -17,15 +17,18 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.*;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -40,7 +43,7 @@ public class Main extends Application {
 	
 	static int screenWidth = 1200;
 	static int screenHeight = 600;
-    static AnchorPane pane = new AnchorPane();
+    static GridPane pane = new GridPane();
     static Scene scene = new Scene(pane, screenWidth, screenHeight);
     static Stage stage;
 	StackPane pane2 = new StackPane();
@@ -61,7 +64,7 @@ public class Main extends Application {
 
  class Homepage {
 	
-	static AnchorPane pane = new AnchorPane();
+	static GridPane pane = new GridPane();
     static Scene scene = new Scene(pane, Main.screenWidth, Main.screenHeight);
     static Stage stage;
 	private Game game;
@@ -71,8 +74,12 @@ public class Main extends Application {
 		game = new Game();
 		bestscore = 0;
 	}
-	public void displayMainmenu(Stage theStage, AnchorPane pane, Scene scene){
+	public void displayMainmenu(Stage theStage, GridPane pane, Scene scene){
 		stage = theStage;
+		pane.setAlignment(Pos.CENTER);
+	    pane.setHgap(10);
+	    pane.setVgap(10);
+	    pane.setPadding(new Insets(25, 25, 25, 25));
 		VBox vbox = new VBox(5);
 		Text t = new Text();
 		t = new Text (10, 20, "Welcome to Color Switch!\n");
@@ -91,14 +98,12 @@ public class Main extends Application {
 
 		vbox.getChildren().addAll(t, btn1, btn2, btn3);
 
-		AnchorPane.setTopAnchor(vbox, 10d);
-		AnchorPane.setLeftAnchor(vbox, 10d);
-
 		pane.getChildren().addAll(vbox);
 		theStage.setTitle("Colour Switch");
 		theStage.setScene(scene);
 		theStage.show();
 	}
+	
 	private void startNewgame(Stage stage) throws FileNotFoundException{	
 		game.start(stage);
 	}
@@ -108,8 +113,41 @@ public class Main extends Application {
 	private void exit(){
 		
 	}
-	public void showSavedgames(Stage stage){
+	public void showSavedgames(Stage theStage){
+		stage = theStage;
+		pane.setAlignment(Pos.CENTER);
+	    pane.setHgap(10);
+	    pane.setVgap(10);
+	    pane.setPadding(new Insets(25, 25, 25, 25));
+		VBox vbox = new VBox(5);
+		Text t = new Text();
+		t = new Text (10, 20, "Saved Games\n");
+		t.setFont(Font.font ("Montserrat", 20));
+		t.setFill(Color.BLACK);
+		Text t2 = new Text();
+		t2 = new Text (10, 20, "Choose a game to start playing:\n");
+		t2.setFont(Font.font ("Montserrat", 15));
+		t2.setFill(Color.BLACK);
+
+		Button btn1 = new Button("Game 1");
+		Button btn2 = new Button("Game 2");
+		Button btn3 = new Button("Game 3");
 		
+		Button btn4 = new Button("Back");
+
+		buttonHandler bh = new buttonHandler();
+
+		btn1.setOnAction(bh);
+		btn2.setOnAction(bh);
+		btn3.setOnAction(bh);
+		btn4.setOnAction(bh);
+
+		vbox.getChildren().addAll(t, t2, btn1, btn2, btn3, btn4);
+
+		pane.getChildren().addAll(vbox);
+		theStage.setTitle("Colour Switch");
+		theStage.setScene(scene);
+		theStage.show();
 	}
 	public int getBestscore(){
 		return this.bestscore;
@@ -128,7 +166,8 @@ public class Main extends Application {
         	}
         	else if(src.getText().equals("Start new game")) {
         		
-        		pane.getChildren().clear();
+        		 pane.getChildren().clear();
+        		 stage.close();
         		 stage = new Stage();
         		try {
 					startNewgame(stage);
@@ -140,6 +179,11 @@ public class Main extends Application {
         	else if(src.getText().equals("Resume a saved game")) {
         		pane.getChildren().clear();
         		showSavedgames(stage);
+        	}
+        	else if(src.getText().equals("Back")) {
+        		stage.close();
+        		pane.getChildren().clear();
+        		displayMainmenu(stage, pane, scene);
         	}
         }
 	}
@@ -166,7 +210,6 @@ class Game extends Application{
     private int colour=0;
     Button button2;
     Button btn;
-    Label l;
     Group root;
     int x3=0;
     int angle=0;
@@ -174,7 +217,7 @@ class Game extends Application{
     Canvas canvas;
     Stage stage;
     Scene scene;// = new Scene(pane, 500, 500);
-    AnchorPane pane;
+    GridPane pane;
     static Timeline timeline;
     StackPane stack = new StackPane();
     StackPane stack2 = new StackPane();
@@ -192,18 +235,20 @@ class Game extends Application{
         canvas = new Canvas( Main.screenWidth, Main.screenHeight );
         root.getChildren().add( canvas );
         button2 = new Button("Pause");
+        button2.setLayoutX(1000);
+        button2.setLayoutY(250);
         root.getChildren().add( button2 ); 
-         btn = new Button();
+        btn = new Button();
         btn.setText("Save");
-        btn.setLayoutX(250);
-        btn.setLayoutY(0);
+        btn.setLayoutX(1005);
+        btn.setLayoutY(300);
         root.getChildren().add(btn);
         gc = canvas.getGraphicsContext2D();
         ball=new Ball();
         obstacle1 = new Obstacle1();
+//      obstacle1.ob1.setLayoutX(1000);
         obstacle2 = new Obstacle2();
-        l = new Label("button not selected");
-        root.getChildren().add(l);
+//      obstacle2.ob2.setLayoutX(1000);
         //root.getChildren().remove(l);
         final long startNanoTime = System.nanoTime();
         //canvas.setOnMouseClicked(event);
@@ -216,29 +261,24 @@ class Game extends Application{
         btn.setOnAction(event2);
         
         
-        stack.getChildren().addAll(obstacle1.ob1 );
-        stack.setLayoutX(30);
-        stack.setLayoutY(30);
+        stack.getChildren().addAll(obstacle1.getOb1() );
+        stack.setLayoutX(Main.screenWidth/2 - obstacle1.getOb1width()/2);
+        stack.setLayoutY(300);
         root.getChildren().add(stack);
         
-        stack2.getChildren().addAll( obstacle2.ob1);
-        stack2.setLayoutX(40);
-        stack2.setLayoutY(40);
+        stack2.getChildren().addAll( obstacle2.getOb2());
+        stack2.setLayoutX(Main.screenWidth/2 - obstacle2.getOb2width());
+        stack2.setLayoutY(400);
         root.getChildren().add(stack2);
         theStage.show();
-        // root.getChildren().add( button3 );
-	    
-        //timer.start(); 
-	    
-        
-        
         timer.start(); 
     }
 	
 	private AnimationTimer timer = new AnimationTimer()
     {
 		   public void handle(long currentNanoTime)
-	        {// background clears canvas
+	        {
+			   // background clears canvas
 	            gc.clearRect(0, 0,512,512);
 
 	            if (pause) {
@@ -251,54 +291,49 @@ class Game extends Application{
 	            	Duration rotateDuration = Duration.millis(3);
 	            	
 	        	    Rotate rotate = new Rotate(0, 100, 100, 0, Rotate.Y_AXIS);
-	        	   // obstacle1.ob1.getTransforms().add(rotate);
+	        	    // obstacle1.ob1.getTransforms().add(rotate);
 	        	    
 	        	    int ddd= 300;
 	        	    long t2 = System.nanoTime() - startNanoTime;
 	        	    timeline = new Timeline( 
-	        	    		new KeyFrame(Duration.ZERO, new KeyValue(obstacle1.ob1.rotateProperty(), angle)), // initial rotate
-	        	            new KeyFrame(rotateDuration, new KeyValue(obstacle1.ob1.rotateProperty(), angle+2)),
-	        	            new KeyFrame(Duration.ZERO, new KeyValue(obstacle1.ob1.translateYProperty(), angle2)),
-	        	            new KeyFrame(rotateDuration, new KeyValue(obstacle1.ob1.translateYProperty(), angle2+2)) ,
+	        	    		new KeyFrame(Duration.ZERO, new KeyValue(obstacle1.getOb1().rotateProperty(), angle)), // initial rotate
+	        	            new KeyFrame(rotateDuration, new KeyValue(obstacle1.getOb1().rotateProperty(), angle+2)),
+	        	            new KeyFrame(Duration.ZERO, new KeyValue(obstacle1.getOb1().translateYProperty(), angle2)),
+	        	            new KeyFrame(rotateDuration, new KeyValue(obstacle1.getOb1().translateYProperty(), angle2+2)) ,
 	        	            
-	        	            new KeyFrame(Duration.ZERO, new KeyValue(obstacle2.ob1.rotateProperty(), angle )), // initial rotate
-	        	            new KeyFrame(rotateDuration, new KeyValue(obstacle2.ob1.rotateProperty(), angle+2 )),
-	        	            new KeyFrame(Duration.ZERO, new KeyValue(obstacle2.ob1.translateYProperty(), angle2 - ddd)),
-	        	            new KeyFrame(rotateDuration, new KeyValue(obstacle2.ob1.translateYProperty(), angle2+2 - ddd))
+	        	            new KeyFrame(Duration.ZERO, new KeyValue(obstacle2.getOb2().rotateProperty(), angle )), // initial rotate
+	        	            new KeyFrame(rotateDuration, new KeyValue(obstacle2.getOb2().rotateProperty(), angle+2 )),
+	        	            new KeyFrame(Duration.ZERO, new KeyValue(obstacle2.getOb2().translateYProperty(), angle2 - ddd)),
+	        	            new KeyFrame(rotateDuration, new KeyValue(obstacle2.getOb2().translateYProperty(), angle2+2 - ddd))
 	        	            );
 	        	    angle=angle+2;
 	        	    
 	                timeline.setCycleCount(Timeline.INDEFINITE);
 	                timeline.setAutoReverse(false);
 	                timeline.play();
-	            double t = (currentNanoTime - startNanoTime) / 1000000000.0; 
-	 
-	            double x = 150; //set to middle of page
-	
-	 	 if (y<350 && y>150  )
-	 		 y=y+2*leftPaddleDY+1;
-	 	 else if (y>=250)
-	 	 {
-	 		angle2=(int) (angle2-leftPaddleDY -1);
-	 		y=y+leftPaddleDY;
-	 		
-	 	 }
-	 	else if (y<250)
-	 		{
-	 		angle2=(int) (angle2-2*leftPaddleDY );
-	 		y=y+1;
-	 		
-	 		}
+	                
+		            double t = (currentNanoTime - startNanoTime) / 1000000000.0; 
+		            double x = 400;
+		            //double x = Main.screenWidth/2 - ball.ballimg.getFitWidth(); //set to middle of page
 		
-	 Random rand = new Random(); 
-	 if (y>=200 && y<=207) {
-			 ball.change_colour(200, 207);
-		 
-	 }
-	   
-	 gc.drawImage(ball.get_ball(), x, y);
-	 }
-	 
+				 	 if (y<350 && y>150  )
+				 		 y=y+2*leftPaddleDY+1;
+				 	 else if (y>=250) {
+				 		angle2=(int) (angle2-leftPaddleDY -1);
+				 		y=y+leftPaddleDY;
+				 	 }
+				 	 else if (y<250)	{
+				 		angle2=(int) (angle2-2*leftPaddleDY );
+				 		y=y+1;
+				 	 }
+			
+					 Random rand = new Random(); 
+					 if (y>=200 && y<=207) {
+						 ball.change_colour(200, 207);
+					 }
+					 gc.drawImage(ball.get_ball(), x, y);
+					 
+				 }
 	        }
     };
 
@@ -313,7 +348,16 @@ class Game extends Application{
         	}
         	else if(src.getText().equals("Save game")) {
         		pane.getChildren().clear();
-        		//startNewgame(stage);
+        		stage.close();
+        		saveGame(stage);
+        	}
+        	else if(src.getText().equals("Save and exit")) {
+        		stage.close();
+        		Platform.exit();
+        	}
+        	else if(src.getText().equals("Back")) {
+        		stage.close();
+        		showSaveresmenu(stage);
         	}
         }
     }
@@ -336,7 +380,6 @@ class Game extends Application{
             switch (event.getCode()) {
                 case UP:
                     leftPaddleDY = -2;
-                    l.setText("  up   "); 
                     break;
                 case P:
                    pause =!(pause);
@@ -348,8 +391,7 @@ class Game extends Application{
         public void handle(ActionEvent e) 
         { 
 	        x3++;
-	        l.setText(String.valueOf(x3));
-	       
+	        //l.setText(String.valueOf(x3));
 	        pause=!(pause);
 	        
         } 
@@ -358,23 +400,56 @@ class Game extends Application{
         public void handle(ActionEvent e) 
         { 
 	        x3++;
-	        l.setText(String.valueOf(x3));
-	       
+	        //l.setText(String.valueOf(x3));
 	        stage = new Stage(); 
-	        showMainmenu(stage);
-	        
+	        showSaveresmenu(stage);
 	        // l.setText("   button   selected    "); 
 	        pause=!(pause);
 	        //Platform.exit();
         } 
     };
-    private void showMainmenu(Stage theStage) {
-    	pane = new AnchorPane();
+    public void saveGame(Stage theStage){
+    	pane = new GridPane();
+    	pane.setAlignment(Pos.CENTER);
+	    pane.setHgap(10);
+	    pane.setVgap(10);
+	    pane.setPadding(new Insets(25, 25, 25, 25));
+    	Scene scene = new Scene(pane, Main.screenWidth, Main.screenHeight);
+		VBox vbox = new VBox();
+		Text t = new Text();
+		t = new Text (10, 20, "Save Game\n");
+		t.setFont(Font.font ("Montserrat", 20));
+		t.setFill(Color.BLACK);
+		
+		Text t2 = new Text();
+		t2 = new Text (10, 20, "Enter a name for the saved game:\n");
+		t2.setFont(Font.font ("Montserrat", 15));
+		t2.setFill(Color.BLACK);
+		
+		TextField tf = new TextField();
+		Button btn1 = new Button("Save and exit");
+		Button btn2 = new Button("Back");
+		buttonHandler bh = new buttonHandler();
+
+		btn1.setOnAction(bh);
+		btn2.setOnAction(bh);
+		vbox.getChildren().addAll(t, t2, tf, btn1, btn2);
+		pane.getChildren().addAll(vbox);
+		theStage.setTitle("Colour Switch: Save game");
+		theStage.setScene(scene);
+		theStage.show();
+	}
+    private void showSaveresmenu(Stage theStage) {
+    	pane = new GridPane();
+    	pane.setAlignment(Pos.CENTER);
+	    pane.setHgap(10);
+	    pane.setVgap(10);
+	    pane.setPadding(new Insets(25, 25, 25, 25));
     	Scene scene = new Scene(pane, Main.screenWidth, Main.screenHeight);
 		VBox vbox = new VBox(5);
         stage = theStage;
         Text t = new Text();
-    	t = new Text (10, 20, "Welcome to Color Switch!\n");
+    	t = new Text (10, 20, "Color Switch\n");
     	t.setFont(Font.font ("Montserrat", 20));
     	t.setFill(Color.BLACK);
     	Button btn1 = new Button("Save game");
@@ -385,13 +460,12 @@ class Game extends Application{
        
         btn3.setOnAction(bh);
         vbox.getChildren().addAll(t, btn1, btn3);
-        AnchorPane.setTopAnchor(vbox, 10d);
-        AnchorPane.setLeftAnchor(vbox, 10d);
         pane.getChildren().addAll(vbox);
         theStage.setTitle("Colour Switch");
         theStage.setScene(scene);
         theStage.show();
 	}
+
 }
 class Ball{
 	private int x;
@@ -402,6 +476,7 @@ class Ball{
     Image blue;
     Image green;
     ImageView ballimg;
+    static private int ballwidth;
     
     public Ball() throws FileNotFoundException {
     	red = new Image("file:images/red.png");
@@ -409,11 +484,11 @@ class Ball{
     	blue = new Image("file:images/blue.png");
     	green = new Image("file:images/green.png");
     	
-         ballimg = new ImageView(this.get_ball());
-         ballimg.setX(600-Main.screenWidth);
-         ballimg.setY(300-Main.screenHeight);
-         ballimg.setFitWidth(30);
-         ballimg.setPreserveRatio(true);
+        ballimg = new ImageView(this.get_ball());
+        ballwidth = 10;
+        ballimg.setFitWidth(this.ballwidth);
+        ballimg.setPreserveRatio(true);
+        
     }
     public void change_colour(int a, int b) {
 		Random rand = new Random(); 
@@ -553,16 +628,31 @@ abstract class Obstacle {
 }
 class Obstacle1 extends Obstacle {
 	
-	static Image singlecircle;
-	static ImageView ob1;
+	static private Image singlecircle;
+	static private ImageView ob1;
+	static private int ob1width;
 	
 	public Obstacle1 () throws FileNotFoundException{
-		this.singlecircle = new Image("file:images/Colour Changer.png");
+		this.singlecircle = new Image("file:images/obstacle1.png");
 		this.ob1 = new ImageView(this.singlecircle);
-		ob1.setFitWidth(150);
-		ob1.setFitHeight(150);
+		ob1width = 200;
+		ob1.setFitWidth(ob1width);
+		ob1.setPreserveRatio(true);
 	}
 	
+	public static ImageView getOb1() {
+		return ob1;
+	}
+
+	public static int getOb1width() {
+		return ob1width;
+	}
+
+	public void setLayoutX(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
 	@Override
 	protected void movement(float duration, Canvas canvas) {
 	
@@ -575,20 +665,31 @@ class Obstacle1 extends Obstacle {
 }
 class Obstacle2 extends Obstacle {
 	
-	static Image singlecircle;
-	static ImageView ob1;
+	static private Image singlecircle;
+	static private ImageView ob2;
+	static private int ob2width;
 	
 	public Obstacle2() throws FileNotFoundException{
-		this.singlecircle = new Image("file:images/Colour Changer.png");
-		this.ob1 = new ImageView(this.singlecircle);
-		 ob1.setX(600-Main.screenWidth);
-         ob1.setY(300-Main.screenHeight);
-         ob1.setFitWidth(100);
-         ob1.setPreserveRatio(true);
-		//ob1.setFitWidth(150);
-		//ob1.setFitHeight(150);
+		this.singlecircle = new Image("file:images/obstacle2.png");
+		this.ob2 = new ImageView(this.singlecircle);
+		 ob2.setX(600-Main.screenWidth);
+         ob2.setY(300-Main.screenHeight);
+         ob2width = 200;
+         ob2.setFitWidth(getOb2width());
+         ob2.setPreserveRatio(true);
 	}
-	
+
+
+	public static ImageView getOb2() {
+		return ob2;
+	}
+
+
+	public static int getOb2width() {
+		return ob2width;
+	}
+
+
 	@Override
 	protected void movement(float duration, Canvas canvas) {
 	
@@ -599,3 +700,4 @@ class Obstacle2 extends Obstacle {
 		
 	}
 }
+
